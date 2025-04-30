d10db5cc8c72fa11f84b653fb3c4368cfbc4e118
vincent <vincentb0824@gmail.com>
2025-04-30 14:40:01 -0700

    peakMetric update

--- call_metrics/peak_performance/views.py
+++ call_metrics/peak_performance/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+def home(request):
+    return render(request, 'home.html', {})
+
+def register(request):
+    form = UserCreationForm()
+    return render(request, 'register.html', {"form":form})
+
+def login(request):
+    return render(request, 'login.html', {})
+
+
