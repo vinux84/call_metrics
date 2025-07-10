9c94f3277405f8a0a5b86cd2be079838702e5a8c
vincent <vincentb0824@gmail.com>
2025-07-09 19:27:38 -0700

    more changes to model

--- call_metrics/peak_performance/models.py
+++ call_metrics/peak_performance/models.py
@@ -19,7 +19,11 @@ class CallLog(models.Model):
     log_date = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f'Call for {self.project.name} on {self.log_date.strftime("%Y-%m-%d %H:%M")} - {"Successful" if self.call_status else "Unsuccessful"}'
+        status_text = "Successful" if self.call_status else "Unsuccessful"
+        callback_info = ""
+        if self.requires_callback:
+            callback_info = f" - Callback (Ext: {self.callback_number or 'N/A'})"
+        return f'Call ID: {self.call_id} for {self.project.name} on {self.log_date.strftime("%Y-%m-%d %H:%M")} - {status_text}{callback_info}'
 
 
 
