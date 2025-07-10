b87b44ff7ad6420076f0878cf90e7f1d13512a00
vincent <vincentb0824@gmail.com>
2025-07-09 19:17:31 -0700

    change to model

--- call_metrics/peak_performance/models.py
+++ call_metrics/peak_performance/models.py
@@ -12,7 +12,14 @@ class Project(models.Model):
     
 class CallLog(models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
-    log_date = models.DateField
+    call_id = models.BigAutoField(primary_key=True)
+    call_status = models.BooleanField(null=False, blank=False)
+    requires_callback = models.BooleanField(default=False)
+    callback_number = models.CharField(max_length=4, blank=True, null=True)
+    log_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'Call for {self.project.name} on {self.log_date.strftime("%Y-%m-%d %H:%M")} - {"Successful" if self.call_status else "Unsuccessful"}'
 
 
 
